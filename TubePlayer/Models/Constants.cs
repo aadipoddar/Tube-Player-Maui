b7de7c34679b7245693a81ea36b0adf72bda2f2b
Aadi Poddar <aadipoddarmail@gmail.com>
2022-10-08 01:17:36 +0530

    Helper, Enums, Constants
    - Service Helper to Auto hook View Models with Views
    - Basic Enums for Navigation Bar, to show what type of Page Mode the App is in
    - Constants Class to store Basic Information about the App To be Reused Frequently

---
+++ TubePlayer/Models/Constants.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiTubePlayer.Models;
+
+public static class Constants
+{
+    public static string ApplicationName = "Tube Player";
+    public static string EmailAddress = @"aadipoddarmail@gmail.com";
+    public static string ApplicationId = "aadi.tubeplayer.app";
+    public static string ApiServiceURL = @"https://youtube.googleapis.com/youtube/v3/";
+    public static string ApiKey = @"AIzaSyAIUijE78_dLz3JheEcHk259TijbU2iV3Y";
+
+
+    public static uint MicroDuration { get; set; } = 100;
+    public static uint SmallDuration { get; set; } = 300;
+    public static uint MediumDuration { get; set; } = 600;
+    public static uint LongDuration { get; set; } = 1200;
+    public static uint ExtraLongDuration { get; set; } = 1800;
+}
