ba44c8a5a109fdf928f7509997a32a6d8f24f86c
Aadi Poddar <aadipoddarmail@gmail.com>
2022-10-08 00:43:57 +0530

    List Extensions and Converters
    - Inverse Converter to negate the boolean value
    - Element Converter to take a list and check is Empty
    - List Extension to show a Random List value for a given List and Add Range

--- Maui.Apps.Framework/GlobalUsings.cs
+++ Maui.Apps.Framework/GlobalUsings.cs
@@ -1,5 +1,7 @@
 ﻿global using System.Text;
 global using System.Text.Json;
+global using System.Collections.ObjectModel;
+global using System.Globalization;
 
 global using Maui.Apps.Framework.Exceptions;
 global using Maui.Apps.Framework.Extensions;
