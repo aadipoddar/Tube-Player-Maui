7ee89ff96ca3905281750411b5627dbaa8a6b9c4
Aadi Poddar <aadipoddarmail@gmail.com>
2022-10-08 21:24:56 +0530

    Base View Model
    - Going Back on Pressing Back Button
    - Closing Modal on Pressing Close Button

--- TubePlayer/GlobalUsings.cs
+++ TubePlayer/GlobalUsings.cs
@@ -1,4 +1,9 @@
 ﻿global using System.Text.Json.Serialization;
 
+global using CommunityToolkit.Mvvm.Input;
+
 global using TubePlayer.Views;
 global using TubePlayer.Models;
+global using TubePlayer.IServices;
+
+global using Maui.Apps.Framework.MVVM;
