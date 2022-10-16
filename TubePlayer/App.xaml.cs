6c78f76260cd9ba85ec017447517b2459181b500
Aadi Poddar <aadipoddarmail@gmail.com>
2022-10-17 00:46:55 +0530

    Fixed Bugs
    - Window Height
    - Fixed Android not showing all videos

--- TubePlayer/App.xaml.cs
+++ TubePlayer/App.xaml.cs
@@ -9,7 +9,7 @@ namespace TubePlayer;
 public partial class App : Application
 {
     const int WindowWidth = 540;
-    const int WindowHeight = 1000;
+    const int WindowHeight = 750;
 
     public App()
     {
