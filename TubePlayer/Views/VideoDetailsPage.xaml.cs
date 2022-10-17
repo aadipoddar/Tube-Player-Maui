899d829e65dc54cca2da4658ee3fb34adac4b98b
Aadi Poddar <aadipoddarmail@gmail.com>
2022-10-17 22:11:10 +0530

    Animations
    Beautiful appearing animations for video details Page

--- TubePlayer/Views/VideoDetailsPage.xaml.cs
+++ TubePlayer/Views/VideoDetailsPage.xaml.cs
@@ -5,5 +5,47 @@ public partial class VideoDetailsPage : ViewBase<VideoDetailsPageViewModel>
     public VideoDetailsPage(object initParams) : base(initParams)
     {
 		InitializeComponent();
-	}
+
+        this.ViewModelInitialized += (s, e) =>
+        {
+            (this.BindingContext as VideoDetailsPageViewModel).DownloadCompleted += VideoDetailsPage_DownloadCompleted;
+        };
+    }
+
+    protected override void OnDisappearing()
+    {
+        (this.BindingContext as VideoDetailsPageViewModel).DownloadCompleted -= VideoDetailsPage_DownloadCompleted;
+
+        base.OnDisappearing();
+    }
+
+    private void VideoDetailsPage_DownloadCompleted(object sender, EventArgs e)
+    {
+        //Video information downloaded. Start showing items.
+
+        if ((this.BindingContext as VideoDetailsPageViewModel).IsErrorState)
+            return;
+
+        if (this.AnimationIsRunning("TransitionAnimation"))
+            return;
+
+
+        var parentAnimation = new Animation();
+
+        //Poster Image Animation
+        parentAnimation.Add(0.0, 0.7,
+            new Animation(v => HeaderView.Opacity = v, 0, 1, Easing.CubicIn));
+
+        //Video Title Container Animation
+        parentAnimation.Add(0.4, 0.7,
+            new Animation(v => VideoTitle.Opacity = v, 0, 1, Easing.CubicIn));
+        
+        //Commit the animation
+        parentAnimation.Commit(this, "TransitionAnimation", 16, Constants.ExtraLongDuration, null,
+            (v, c) =>
+            {
+                //Action to perform on completion (if any)
+            });
+
+    }
 }
